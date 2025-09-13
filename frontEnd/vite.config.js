d4bf951de528152d0f9a33c634ebf9543904659c
Jawahar Lingam <jawahar.lee03@gmail.com>
2025-09-13 10:21:49 +0530

    installed tailwindCSS for styling

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   base: process.env.NODE_ENV === "production" ? "/smart-fixly/" : "/",
   build: {
     outDir: "../docs",  // stores builds inside docs folder to be rendered in github pages(development branch)
